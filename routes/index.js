1875ee643aafeb3bfe662de72525d625c4c50d60
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-08-26 13:50:07 +0100

    auth route skeleton added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,54 @@ router.get('/', (req, res, next) => {
   return res.status(200).json({ msg: 'Hello World' })
 });
 
+router.get('/register', (req, res, next) => {
+  return res.status(200).json({ msg: 'Get User registered' })
+});
+
+
+router.post('/register', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Get logged in user
+router.get('/login', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Log In User
+router.post('/login', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Get Profile
+router.get('/profile', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Update Profile
+router.put('/profile/:user_id', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Forgot Password
+router.get('/forgot_pw', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Forgot Password
+router.put('/forgot_pw', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Reset Password
+router.get('/reset_pw/:token', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+// Reset Password
+router.put('/reset_pw/:token', (req, res, next) => {
+  return res.status(200).json({ msg: 'User registered' })
+});
+
+
 module.exports = router;
