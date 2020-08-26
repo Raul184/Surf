60c9796ef461c71ba34b63a1014078b171908566
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-08-26 13:00:07 +0100

    routes eskeleton set

---
+++ routes/posts.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+
+// Get Posts
+router.get('/' , (req, res, next) => {
+  res.send('/posts')
+})
+
+// Get Posts New
+router.get('/new', (req, res, next) => {
+  res.send('/posts/new')
+})
+
+// Create Posts
+router.post('/', (req, res, next) => {
+  res.send('/CREATE posts')
+})
+
+// Get 1 Post : id
+router.get('/:id', (req, res, next) => {
+  res.send('SHOW /posts/:id')
+})
+
+// EDIT Posts
+router.get('/:id/edit', (req, res, next) => {
+  res.send('EDIT /posts/:id/edit')
+})
+
+// Put Posts
+router.put('/:id', (req, res, next) => {
+  res.send('Update /posts/:id')
+})
+
+// Delete Posts
+router.delete('/id', (req, res, next) => {
+  res.send('DELETE /posts/:id')
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -19,6 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
 
 app.use('*' , ( req , res , next ) => {
   return res.status(404).json({ msg: 'Sorry , page not found'})
