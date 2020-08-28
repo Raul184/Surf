24da464922bb03d357aff9837d563658b1e88670
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-08-28 11:38:44 +0100

    user Model set

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose'); 
+const Schema = mongoose.Schema
+
+const usersSchema = new Schema({
+  email: String,
+  image: String,
+  posts: [
+    { 
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    }
+  ]
+})
+usersSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User' , usersSchema)
