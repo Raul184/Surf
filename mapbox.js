578c856570939cc92577fb3b85782dd5058873d5
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-09-14 14:13:23 +0100

    mapbox geoCoding client set

--- mapbox.js
+++ mapbox.js
@@ -2,3 +2,18 @@ require('dotenv').config()
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
 const geocodingClient = mbxGeocoding({ accessToken: process.env.MB });
 
+async function geoCoder(location){
+  try {
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: location,
+        limit: 1,
+      })
+      .send();
+    console.log(response.body.features[0].geometry.coordinates);
+  } 
+  catch (err) {
+    console.log(err.message)  
+  }
+}
+geoCoder('Alaska', 'US')
